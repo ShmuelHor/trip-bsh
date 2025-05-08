f60d0ee79f16205177be1423719a918e6d54ccc7
ShmuelHor <shmuel.hor@gmail.com>
2025-05-08 12:19:26 +0300

    clean main branch

--- src/express/authentication/router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { validateRequest, wrapController } from '../../utils/express/wrappers';
-import { UsersController } from '../users/controller';
-import { AuthenticationController } from './controller';
-import { loginRequestSchema, registerRequestSchema } from './validations';
-
-export const authenticationRouter = Router();
-authenticationRouter.post('/login', validateRequest(loginRequestSchema), wrapController(AuthenticationController.login));
-authenticationRouter.post('/register', validateRequest(registerRequestSchema), wrapController(UsersController.createOne));
--- src/types/express/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { UserDocument } from '../../express/users/interface';
-
-declare module 'express-serve-static-core' {
-    interface Request {
-        user: UserDocument;
-    }
-}
