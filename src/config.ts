0d5b206e12c9ca4d0b7e62b771d4f84c353f068e
ShmuelHor <shmuel.hor@gmail.com>
2025-05-19 10:47:44 +0300

    FIX

--- src/config.ts
+++ src/config.ts
@@ -8,21 +8,21 @@ export const config = {
     users: {
         baseRoute: env.get('USERS_BASE_ROUTE').default('/api/users').asString(),
         mongo: {
-            uri: env.get('USERS_MONGO_URI').required().asString(),
+            uri: env.get('MONGO_URI').required().asString(),
             usersCollectionName: env.get('USERS_COLLECTION_NAME').default('users').asString(),
         },
     },
     trips: {
         baseRoute: env.get('TRIPS_BASE_ROUTE').default('/api/trips').asString(),
         mongo: {
-            uri: env.get('TRIPS_MONGO_URI').required().asString(),
+            uri: env.get('MONGO_URI').required().asString(),
             tripsCollectionName: env.get('TRIPS_COLLECTION_NAME').default('trips').asString(),
         },
     },
     payments: {
         baseRoute: env.get('PAYMENTS_BASE_ROUTE').default('/api/payments').asString(),
         mongo: {
-            uri: env.get('PAYMENTS_MONGO_URI').required().asString(),
+            uri: env.get('MONGO_URI').required().asString(),
             paymentsCollectionName: env.get('PAYMENTS_COLLECTION_NAME').default('payments').asString(),
         },
     },
