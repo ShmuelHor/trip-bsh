59e1ef717fb80c6f4bb76c296d4e0238c1d7574e
ShmuelHor <shmuel.hor@gmail.com>
2025-05-29 17:48:33 +0300

    c

--- src/index.ts
+++ src/index.ts
@@ -12,13 +12,16 @@ const app = express();
 const port = config.service.port;
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173',"*"],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 mongoose
     .connect(config.users.mongo.uri)
     .then(() => console.log('✅ Connected to MongoDB'))
