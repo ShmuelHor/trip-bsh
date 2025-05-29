26c15432b6a18b06eafe486dc419c2c674c327c1
ShmuelHor <shmuel.hor@gmail.com>
2025-05-29 18:07:28 +0300

    A

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 const port = config.service.port;
 
 app.use(cors({
-    origin: ['http://localhost:5173',"*"],
+    origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
